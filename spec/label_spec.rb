c8b29f0c55845d093ae45cf6572e8662b840a2d8
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-04-24 21:34:40 +0100

    Add tests for label add_item method

---
+++ spec/label_spec.rb
@@ -0,0 +1,22 @@
+require_relative '../Classes/label'
+require_relative '../Classes/book'
+
+describe Label do
+  let(:book1) { Book.new(1, 'Fantasy', 'J.R.R.', 'Tolkien', 'The Lord of the Rings', 'The Hobbit', 'red', 'bad', 'Allen & Unwin', '2010-10-12') }
+  let(:book2) { Book.new(1, 'Comic', 'Donald', 'Trump', 'American Savage', 'Washington', 'blue', 'good', 'Barrack Obama', '2022-10-12') }
+
+  before(:each) do
+    @label = Label.new(1, 'The Hobbit', 'red')
+  end
+  context 'when creating a new label' do
+    it 'should create a new label' do
+      @label.add_item(book1)
+      expect(@label.items).to include(book1)
+    end
+
+    it 'should create a second new label' do
+      @label.add_item(book2)
+      expect(@label.items).to include(book2)
+    end
+  end
+end
