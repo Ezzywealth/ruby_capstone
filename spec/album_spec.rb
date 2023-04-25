2c8adcfa02858ae2a81e24468b876e81a9d84905
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-04-25 11:41:05 +0100

    Merge Marko test with Ezekiel test

--- spec/album_spec.rb
+++ spec/album_spec.rb
@@ -1,31 +1,41 @@
 require_relative '../Classes/album'
 require_relative '../Classes/item'
+require_relative '../Classes/genre'
+require_relative '../Classes/label'
 
 describe MusicAlbum do
+  let(:label) do
+    Label.new(nil, 'Mavins', 'Red')
+  end
+  let(:genre) do
+    Genre.new(nil, 'Fantasy')
+  end
+  # id, genre, author, source, label, published_date,
+  #  on_spotify
   describe '#initialize' do
     it 'should set the id, publish_date, archived, and on_spotify attributes' do
-      album = MusicAlbum.new(1, 'Fantasy', 'J.R.R.', 'The Lord of the Rings', 'The Hobbit', 'red',
-                             'Allen & Unwin', '2010-10-12', false)
+      album = MusicAlbum.new(1, genre, 'J.R.R', 'The Hobbit', label,
+                             '2010-10-12', false)
       expect(album.id).to eq(1)
     end
   end
 
   describe '#can_be_archived?' do
     it 'should return true if the item can be archived and is on Spotify' do
-      album = MusicAlbum.new(2, 'Fantasy', 'J.R.R.', 'The Lord of the Rings', 'The Hobbit', 'red',
-                             'Allen & Unwin', '2010-10-12', true)
+      album = MusicAlbum.new(2, genre, 'J.R.R', 'The Hobbit', label,
+                             '2010-10-12', true)
       expect(album.can_be_archived?).to be_truthy
     end
 
     it 'should return false if the item cannot be archived' do
-      album = MusicAlbum.new(3, 'Fantasy', 'J.R.R.', 'The Lord of the Rings', 'The Hobbit', 'red',
-                             'Allen & Unwin', '2010-10-12', false)
+      album = MusicAlbum.new(3, genre, 'J.R.R', 'The Hobbit', label,
+                             '2010-10-12', false)
       expect(album.can_be_archived?).to be_falsey
     end
 
     it 'should return false if the item is not on Spotify' do
-      album = MusicAlbum.new(4, 'Fantasy', 'J.R.R.', 'The Lord of the Rings', 'The Hobbit', 'red',
-                             'Allen & Unwin', '2010-10-12', false)
+      album = MusicAlbum.new(4, genre, 'J.R.R', 'The Hobbit', label,
+                             '2010-10-12', false)
       expect(album.can_be_archived?).to be_falsey
     end
   end
