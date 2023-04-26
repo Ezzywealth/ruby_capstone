a8090509e556b1dda4f559b80e1a2629280ecd54
juharyimami <juharyimami2011@gmail.com>
2023-04-26 08:46:08 +0300

    edited

---
+++ Listing/list_games.rb
@@ -0,0 +1,10 @@
+def list_all_games
+    if @games.empty?
+      puts 'No recorded games yet!'
+      puts
+    else
+      @games.each do |game|
+        puts "Multiplayer: #{game['multiplayer']}, Last Played Date: #{game['last_played_at']}"
+      end
+    end
+  end
