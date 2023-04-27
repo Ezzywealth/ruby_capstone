7be49cc155c736fc148b2ae31715336b8f243792
juharyimami <juharyimami2011@gmail.com>
2023-04-27 11:11:50 +0300

    schema updated

--- Schema/game_schema.sql
+++ Schema/game_schema.sql
@@ -1,32 +1,16 @@
-CREATE TABLE games(
+CREATE TABLE game (
   id SERIAL PRIMARY KEY,
-  genre_id INT,
-  author_id INT,
-  source_id INT,
-  label_id INT,
-  publish_date date,
-  archived boolean,
-  multiplayer boolean,
-  last_played_at date,
-    CONSTRAINT genre_id
-    FOREIGN KEY(genre_id)
-      REFERENCES genre(id)
-      ON DELETE CASCADE,
-  CONSTRAINT source_id
-    FOREIGN KEY(source_id)
-      REFERENCES source(id)
-      ON DELETE CASCADE,
-  CONSTRAINT author_id
-    FOREIGN KEY(author_id)
-      REFERENCES author(id)
-      ON DELETE CASCADE,
-  CONSTRAINT label_id
-    FOREIGN KEY(label_id)
-      REFERENCES label(id)
-      ON DELETE CASCADE
-);
+  source_id INTEGER,
+  label_id INTEGER ,
+  genre_id INTEGER,
+  author_id INTEGER REFERENCES author(id),
+  published_date INTEGER,
+  multiplayer VARCHAR,
+  last_played_at VARCHAR,
+)
 
-CREATE INDEX author_id_asc_games ON games(author_id ASC);
-CREATE INDEX source_id_asc_games ON games(source_id ASC);
-CREATE INDEX genre_id_asc_games ON games(genre_id ASC);
-CREATE INDEX label_id_asc_games ON games(label_id ASC);
+CREATE TABLE author (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR,
+    last_name VARCHAR,
+)
