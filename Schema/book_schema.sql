b6be43c8874ce62b42ca298639af11ebd001cbb7
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-04-27 09:08:05 +0100

    Edit author name

--- Schema/book_schema.sql
+++ Schema/book_schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE book (
     id INTEGER PRIMARY KEY,
     source_id INTEGER,
-    label_id INTEGER REFERENCES label(id),
+    label_id INTEGER REFERENCES labels(id),
     genre_id INTEGER,
     author_id INTEGER,
     published_date INTEGER,
